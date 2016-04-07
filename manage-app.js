ecc0b62a82b7a0d434c4f4e642eb4e01b8609e69
Erika Yoon <erikaY@Erikas-Air.attlocal.net>
2016-04-07 16:04:54 -0700

    removing single quotes inside of command string in manage-app that's causing the prompt to terminate

--- manage-app.js
+++ manage-app.js
@@ -17,8 +17,8 @@ function start(config) {
         "git pull; " +  
         "git checkout %s;" + 
         "npm update 2>&1; " +
-        "echo 'updating server ip to %s'; " +
-        "echo 'export default { API_URL: \"http://%s:%s\" }' > config/client-config.js; " + 
+        "echo updating server ip to %s; " +
+        "echo export default { API_URL: \"http://%s:%s\" } > config/client-config.js; " + 
         'npm start -- --db-address $%s_PORT_27017_TCP_ADDR', 
         config.branch,  // git checkout branch
         config.hostIP, // echo server ip to hostIP
