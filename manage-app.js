ae84ea443a88b6330c5f9045c6e6de851e46fff9
Erika Yoon <erikaY@Erikas-Air.attlocal.net>
2016-04-07 16:10:24 -0700

    adding quotes around echoing the host ip to the client-config.js

--- manage-app.js
+++ manage-app.js
@@ -18,7 +18,7 @@ function start(config) {
         "git checkout %s;" + 
         "npm update 2>&1; " +
         "echo updating server ip to %s; " +
-        "echo export default { API_URL: \"http://%s:%s\" } > config/client-config.js; " + 
+        "echo \"export default { API_URL: \"http://%s:%s\" } \" > config/client-config.js; " + 
         'npm start -- --db-address $%s_PORT_27017_TCP_ADDR', 
         config.branch,  // git checkout branch
         config.hostIP, // echo server ip to hostIP
