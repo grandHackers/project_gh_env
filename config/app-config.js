0b12bc1878099ace6578182413125e3cdbecd5e0
Erika Yoon <erikaY@Erikas-Air.attlocal.net>
2016-04-07 12:54:32 -0700

    adding 'host_ip' setting to such that user can specify the host_ip that will be used by the frontend to make server calls

--- config/app-config.js
+++ config/app-config.js
@@ -4,5 +4,10 @@ module.exports = {
     hostPort: 8080,
     containerPort: 8080,
     link: 'mongo',
-    branch: 'master'
+    branch: 'master',
+    // HACK
+    // currently since api server is not at a fixed address - depends on the host
+    // default value set to localhost, but user should come and change
+    // if he or she intends on accessing the app outside of the host.
+    hostIP: 'localhost'
 };
