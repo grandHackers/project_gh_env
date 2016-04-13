c91cfc95875767b99035dfd984837387add93ea9
Erika Yoon <erikaY@Erikas-Air.attlocal.net>
2016-04-12 23:26:32 -0700

    taking out hostIP for now

--- config/app-config.js
+++ config/app-config.js
@@ -9,5 +9,4 @@ module.exports = {
     // currently since api server is not at a fixed address - depends on the host
     // default value set to localhost, but user should come and change
     // if he or she intends on accessing the app outside of the host.
-    hostIP: 'localhost'
 };
